14f2ee9bebbd593769a6e3e1b34f3f531c312f47
João Victor Antonucci <jvictor.antonucci@gmail.com>
2024-02-23 19:49:24 -0300

    feat: develop bento home layout (WIP)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,23 @@
-<h1 class="grid h-screen place-content-center text-8xl">Work in Progress 🐟</h1>
+<div class="w-[1300]">
+	<section class="py-[200]">
+		<h3 class="text-center text-xl font-extralight font-poppins tracking-normal">
+			Olá, sou o João Victor
+		</h3>
+		<h1 class="text-center text-5xl font-semibold mt-1 mb-2 font-poppins tracking-normal">
+			Software Engineer
+		</h1>
+		<h2 class="text-center text-xl font-normal font-poppins tracking-normal">
+			Focado em desenvolvimento mobile
+		</h2>
+	</section>
+	<section class="grid grid-cols-3 grid-rows-8 grid-flow-row gap-8 justify-items-stretch">
+		<div class="bg-card-color rounded-2xl row-span-2">1</div>
+		<div class="bg-card-color rounded-2xl col-span-2">2</div>
+		<div class="bg-card-color rounded-2xl row-span-4 col-span-2">3</div>
+		<div class="bg-card-color rounded-2xl row-span-2">4</div>
+		<div class="bg-card-color rounded-2xl row-span-2">5</div>
+		<div class="bg-card-color rounded-2xl row-span-2">6</div>
+		<div class="bg-card-color rounded-2xl ">7</div>
+		<div class="bg-card-color rounded-2xl ">8</div>
+	</section>
+</div>
